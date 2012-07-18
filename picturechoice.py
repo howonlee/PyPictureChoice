607cf689be58635362215387caedcdf6069a1556
Howon Lee <hlee.howon@gmail.com>
2012-07-18 11:46:12 -0700

    split into modules, not working thought

--- picturechoice.py
+++ picturechoice.py
@@ -1,5 +1,8 @@
 from Tkinter import *
 from ttk import *
+from block import *
+from choice import *
+from our_break import *
 
 class MainApp:
     def __init__(self, parent):
@@ -19,72 +22,6 @@ class MainApp:
         ''' toBlockClick
          pressed in main screen, goes to block screens '''
         self.container1.pack_forget()
-        block = Block(root)
-
-class Block:
-    def __init__(self, parent):
-        self.myParent = parent
-        self.container1 = Frame(parent)
-        self.container1.pack()
-        self.blockLabel = Label(self.container1)
-        self.blockLabel.configure(text="Block #: 0")
-        self.blockLabel.pack()
-        self.toChoice = Button(self.container1)
-        self.toChoice.configure(text="Press to start block")
-        self.toChoice.bind("<Button-1>", self.toChoiceCallback)
-        self.toChoice.pack()
-
-    def toChoiceCallback(self, event):
-        ''' toChoiceCallback
-        pressed in block screen, goes to choice screen'''
-        self.container1.pack_forget()
-        choice = Choice(root)
-
-class Choice:
-    def __init__(self, parent):
-        self.myParent = parent
-        self.container1 = Frame(parent)
-        self.container1.pack()
-        self.choice1 = Button(self.container1)
-        self.choice2 = Button(self.container1)
-        self.toBreak = Button(self.container1)
-        self.choice1.configure(text="Choice 1")
-        self.choice2.configure(text="Choice 2")
-        self.toBreak.configure(text="Take a Break")
-        self.choice1.bind("<Button-1>", self.choice1Callback)
-        self.choice2.bind("<Button-1>", self.choice2Callback)
-        self.toBreak.bind("<Button-1>", self.toBreakCallback)
-        self.choice1.pack()
-        self.choice2.pack()
-
-    def choice1Callback(self, event):
-        self.choice1.pack_forget()
-        self.choice2.pack_forget()
-        self.toBreak.pack()
-
-    def choice2Callback(self, event):
-        self.choice1.pack_forget()
-        self.choice2.pack_forget()
-        self.toBreak.pack()
-
-    def toBreakCallback(self, event):
-        self.container1.pack_forget()
-        our_break = Break(root)
-
-class Break:
-    def __init__(self, parent):
-        self.myParent = parent
-        self.container1 = Frame(parent)
-        self.container1.pack()
-        self.breakLabel = Label(self.container1)
-        self.breakLabel.configure(text="Take a break")
-        self.toBlock = Button(self.container1)
-        self.toBlock.configure(text="Press to go to the next block")
-        self.toBlock.bind("<Button-1>", self.toBlockCallback)
-        self.toBlock.pack()
-    
-    def toBlockCallback(self, event):
-        self.container1.pack_forget() 
         block = Block(root)
 
 root = Tk()
